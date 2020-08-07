ce66511744e8410a3fe69a27b283c790d1ff7a27
Christian Berger <mail@christianberger.net>
2020-08-07 14:01:11 +0000

    allow xsrf
    Signed-off-by: Christian Berger <mail@christianberger.net>

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 /bin/ip -4 route add 10.0.0.0/16 via 172.99.0.2 dev eth0
-cd /code && python run.py --fbidhttp=False --policy=autoadd
+cd /code && python run.py --fbidhttp=False --policy=autoadd --xsrf=False
